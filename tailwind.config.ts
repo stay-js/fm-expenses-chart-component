535d5bd4aafb7a944a82bb0d2a767f9a2fd3fcfb
Nagy Zétény <nagyzeteny6@gmail.com>
2023-12-01 18:52:59 +0100

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss';
+
+const config = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: 'DM Sans',
+      },
+      colors: {
+        'soft-red': 'hsl(10, 79%, 65%)',
+        cyan: 'hsl(186, 34%, 60%)',
+        'dark-brown': 'hsl(25, 47%, 15%)',
+        'medium-brown': 'hsl(28, 10%, 53%)',
+        cream: 'hsl(27, 66%, 92%)',
+        'very-pale-orange': 'hsl(33, 100%, 98%)',
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
+
+export default config;
